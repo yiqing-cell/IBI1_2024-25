dab7682bf9b93d4c41823e09c72203f199bec681
Shen Yiqing <yiqing.24@intl.zju.edu.cn>
2025-03-11 10:05:03 +0800

    added variables.py

---
+++ Pratical4/variables.py
@@ -0,0 +1,15 @@
+a=15
+b=75
+c=a+b
+
+d=90
+e=5
+f=d+e
+
+if c>f:
+    print("drive to a nearby car park and then walk the final stage is quicker")
+elif c<f:
+    print("walk to the bus stop and take the bus directly to their office is quicker")
+else:
+    print("two ways are same")
+#walk to the bus stop and take the bus directly to their office is quicker
