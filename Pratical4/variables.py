9c5d7efdd45ba083fe1c7df29423c2f6e66e5c18
Shen Yiqing <yiqing.24@intl.zju.edu.cn>
2025-03-11 10:13:40 +0800

    update variables.py

--- Pratical4/variables.py
+++ Pratical4/variables.py
@@ -13,3 +13,13 @@ elif c<f:
 else:
     print("two ways are same")
 #walk to the bus stop and take the bus directly to their office is quicker
+
+X=True
+Y=False
+W= X and Y
+print(W)
+# X      | Y      | W
+# True   | True   | True
+# True   | False  | False
+# False  | True   | False
+# False  | False  | False
