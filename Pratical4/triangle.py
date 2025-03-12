4a3f1c3015f743833b31d57d4ef24caa7bc43eec
Shen Yiqing <yiqing.24@intl.zju.edu.cn>
2025-03-12 14:01:34 +0800

    add information to triangle.py

--- Pratical4/triangle.py
+++ Pratical4/triangle.py
@@ -3,6 +3,6 @@
 # 2. For each number n, calculate the sum of integers from 1 to n
 # 3. Display each calculated triangular number
 
-for n in range(1, 11): #displays the first ten values
-    triangular_number = sum(range(1, n + 1)) # Calculate the sum from 1 to n
+for n in range(1, 11): #Show which triangle this is
+    triangular_number = sum(range(1, n + 1)) # Calculate the sum from 1 to n as the number of triangle
     print(triangular_number) # Display the result
