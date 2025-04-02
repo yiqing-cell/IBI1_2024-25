006ec9726733eaf3881a0eefc8559fb112eeafe9
Shen Yiqing <yiqing.24@intl.zju.edu.cn>
2025-04-02 16:29:50 +0800

    add largest_intron.py

---
+++ Practical7/largest_intron.py
@@ -0,0 +1,15 @@
+import re
+
+seq = 'ATGCAAGTGGTGTGTCTGTTCTGAGAGGGCCTAA'
+
+# find all GT...AG
+matches = re.findall(r'(?=(GT.+?AG))', seq)
+print(matches)
+max_length = 0
+for match in matches:
+    intron_length = len(match) 
+    if intron_length > max_length:
+        max_length = intron_length
+
+print(f"The length of the largest intron is: {max_length}")
+
