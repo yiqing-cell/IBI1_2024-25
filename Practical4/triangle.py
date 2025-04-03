303241e5caa3458ab4033e61f489b447cbad8f83
yiqing-cell <yiqing.24@intl.zju.edu.cn>
2025-04-03 16:05:21 +0800

    Delete Practical4/triangle.py

--- Practical4/triangle.py
+++
@@ -1,8 +0,0 @@
-# Project Plan (Pseudocode):
-# 1. Iterate over numbers 1 to 10 to compute each triangular number
-# 2. For each number n, calculate the sum of integers from 1 to n
-# 3. Display each calculated triangular number
-
-for n in range(1, 11): #Show which triangle this is
-    triangular_number = sum(range(1, n + 1)) # Calculate the sum from 1 to n as the number of triangle
-    print(triangular_number) # Display the result
