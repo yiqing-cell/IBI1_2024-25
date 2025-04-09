31f5328c43a90bcedf2d9159064473f13fc1315d
Shen Yiqing <yiqing.24@intl.zju.edu.cn>
2025-04-09 13:14:27 +0800

    update Restriction_enzyme_cut_sites.py

--- Practical8/Restriction_enzyme_cut_sites.py
+++ Practical8/Restriction_enzyme_cut_sites.py
@@ -1,8 +1,9 @@
+import re
 def find_restriction_sites(dna_sequence, enzyme_sequence):
     dna_sequence = dna_sequence.upper()
     enzyme_sequence = enzyme_sequence.upper()
     
-    if not all(base in "ACGT" for base in dna_sequence) or not all(base in "ACGT" for base in enzyme_sequence):
+    if bool(re.search(r'[^ATCG]', dna_sequence.upper())) == True or bool(re.search(r'[^ATCG]', enzyme_sequence.upper())) == True:
         return "Error: Sequences must contain only A, C, G, T."
     for i in range(len(dna_sequence) - len(enzyme_sequence) + 1):
         if dna_sequence[i:i + len(enzyme_sequence)] == enzyme_sequence:
